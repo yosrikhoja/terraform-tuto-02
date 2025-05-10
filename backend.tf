9f64d9ddd51d88429915d940dd9ed71acde56014
yosrikhoja <khojayosri@gmail.com>
2025-05-10 19:28:36 +0100

    add security group with scipt in th ec2 instance

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-tuto-02"
+    bucket = "terraform-tuto-002"
     key    = "terraform-tuto-02.tfstate"
     region = "us-east-1"
   }
--- main.tf
+++ main.tf
@@ -11,7 +11,29 @@ terraform {
 resource "aws_instance" "example" {
   ami           = "ami-0f88e80871fd81e91" 
   instance_type = "t3.micro"
-  tags = {
+  vpc_security_group_ids = [aws_security_group.allow_http.id]
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y httpd
+              systemctl start httpd
+              systemctl enable httpd
+              echo "Hello, World!" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+              user_data_replace_on_change = true
+    tags = {
     Name = "server"
   }
+}
+resource "aws_security_group" "allow_http" {
+  name        = "allow_http"
+  description = "Allow HTTP traffic"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    }
 }
